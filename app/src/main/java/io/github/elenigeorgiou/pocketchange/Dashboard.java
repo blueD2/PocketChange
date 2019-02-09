741dd7ab137fb9f50c541c36fe3ba1e270e605ca
root <root@NKS.localdomain>
2019-02-09 17:44:35 -0600

    set text group name at top of dash

--- app/src/main/java/io/github/elenigeorgiou/pocketchange/Dashboard.java
+++ app/src/main/java/io/github/elenigeorgiou/pocketchange/Dashboard.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -13,9 +14,15 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
         Intent intent = getIntent();
         String groupID = intent.getStringExtra("groupid");
         GroupSpace currentGS = GroupSpace.getGroupFromID(groupID);
+
+        String currentGroupName = currentGS.getGroupName();
+        TextView groupNameLabel = (TextView) findViewById(R.id.groupName);
+        groupNameLabel.setText(currentGroupName);
+
         ArrayList<ListItem> bucketList = currentGS.getBucketList();
         ArrayAdapter<ListItem> bucketListAdapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_multiple_choice,
