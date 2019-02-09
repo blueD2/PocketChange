aa32046b3ad21bc3a0ddfbebf679132ec5da0239
root <root@NKS.localdomain>
2019-02-09 17:14:17 -0600

    list view on dashboard

--- app/src/main/java/io/github/elenigeorgiou/pocketchange/Dashboard.java
+++ app/src/main/java/io/github/elenigeorgiou/pocketchange/Dashboard.java
@@ -2,6 +2,9 @@ package io.github.elenigeorgiou.pocketchange;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        GroupSpace currentGS = null;
+        ArrayList<ListItem> bucketList = currentGS.getBucketList();
+        ArrayAdapter<ListItem> bucketListAdapter = new ArrayAdapter(null,
+                android.R.layout.simple_list_item_multiple_choice,
+                bucketList);
     }
 }
